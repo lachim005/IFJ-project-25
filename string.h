545d9ac35d1266a2f7a1d4a79f6dbab3fa78dba0
Michal Šebesta <lachim005@seznam.cz>
2025-10-13 19:33:43 +0200

    Adds string.h declaring string operations

---
+++ string.h
@@ -0,0 +1,27 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+#define _STRING_INIT_SIZE 32
+#define _STRING_SCALE_FACTOR 2
+
+typedef struct string {
+    char *val;
+    /// Capacity allocated at *val
+    unsigned capacity;
+    /// Current length of the string without the null terminator
+    unsigned length;
+} String;
+
+/// Creates a new string structure and returns a pointer to it or NULL if allocation failed
+String *str_init();
+
+/// Appends a given string to a string. Returns true if the operation was successfull
+bool str_append_string(String *str, char *str_to_append);
+
+/// Appends a given character to a string. Returns true if the operation was successfull
+bool str_append_char(String *str, char ch);
+
+/// Frees a given string
+void str_free(String **str);
+
+#endif // !_STRING_H_
