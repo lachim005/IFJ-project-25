bca9a0337b2c28bdb30bfa7228bfa89e7e53ea78
Michal Šebesta <lachim005@seznam.cz>
2025-11-16 20:32:29 +0000

    Adds a few tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CORRECT_DIR=correct
+COMP_DIR=comp-
 
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
@@ -12,16 +13,9 @@ EXE=../../main
 INT=../../ic25int-linux-x86_64
 
 run_correct() {
-    echo ======================================
+    echo --------------------------------------
     echo "      RUNNING CORRECT TESTS"
-    echo ======================================
-    err=0
-    comp=0
-    mem=0
-    int=0
-    output_err=0
-    correct=0
-    sum=0
+    echo --------------------------------------
     cd $CORRECT_DIR
     for i in *.test; do
         ((sum++))
@@ -66,33 +60,38 @@ run_correct() {
         echo -e "${GREEN}[ CORRECT ]          " $RESET $i
         ((correct++))
     done
+    cd $OLDPWD
+}
 
-    # Print report
-    echo ======================================
-    if [ $correct -ne 0 ]; then
-        echo -e "           Correct: ${GREEN}${correct}${RESET}/${sum}"
-    fi
-    if [ $mem -ne 0 ]; then
-        echo -e "      Memory error: ${YELLOW}${mem}${RESET}/${sum}"
-    fi
-    if [ $comp -ne 0 ]; then
-        echo -e " Compilation error: ${RED}${comp}${RESET}/${sum}"
-    fi
-    if [ $int -ne 0 ]; then
-        echo -e " Interpreter error: ${RED}${int}${RESET}/${sum}"
-    fi
-    if [ $output_err -ne 0 ]; then
-        echo -e "      Output error: ${RED}${output_err}${RESET}/${sum}"
-    fi
-    if [ $err -ne 0 ]; then
-        echo - - - - - - - - - - - - - - - - - - -
-        echo -e "      Total errors: ${RED}${err}${RESET}/${sum}"
-    fi
-    echo ======================================
+run_with_comp_error() {
+    echo --------------------------------------
+    echo "  RUNNING TESTS WITH COMPILE ERROR $1"
+    echo --------------------------------------
+    cd $COMP_DIR$1
+    for i in *.test; do
+        ((sum++))
+        # Compile
+        $EXE < $i 2> /dev/null 1> /dev/null
+        code=$?
+        if [ $code -ne $1 ]; then
+            echo -e "${RED}[ COMPILATION ERROR ]" $RESET $i "${GRAY}returned $code${RESET}"
+            ((comp++))
+            ((err++))
+            continue
+        fi
 
-    if [ $err -ne 0 ]; then
-        exit 1
-    fi
+        if [ $CHECK_MEM -eq 1 ]; then
+            # Check memory leaks
+            valgrind --leak-check=full --errors-for-leak-kinds=all --error-exitcode=1 $EXE < $i 1>/dev/null 2>/dev/null
+            if [ $? -ne 0 ]; then
+                echo -e "${YELLOW}[ VALGRIND ERROR ]   " $RESET $i
+                ((mem++))
+            fi
+        fi
+
+        echo -e "${GREEN}[ CORRECT ]          " $RESET $i
+        ((correct++))
+    done
 
 
     cd $OLDPWD
@@ -119,6 +118,45 @@ if [ $? -ne 0 ]; then
 fi
 cd $OLDPWD
 
+err=0
+comp=0
+mem=0
+int=0
+output_err=0
+correct=0
+sum=0
+
 # Run correct tests
 run_correct
+run_with_comp_error 2
+run_with_comp_error 3
+run_with_comp_error 4
+run_with_comp_error 5
+
+# Print report
+echo ======================================
+if [ $correct -ne 0 ]; then
+    echo -e "           Correct: ${GREEN}${correct}${RESET}/${sum}"
+fi
+if [ $mem -ne 0 ]; then
+    echo -e "      Memory error: ${YELLOW}${mem}${RESET}/${sum}"
+fi
+if [ $comp -ne 0 ]; then
+    echo -e " Compilation error: ${RED}${comp}${RESET}/${sum}"
+fi
+if [ $int -ne 0 ]; then
+    echo -e " Interpreter error: ${RED}${int}${RESET}/${sum}"
+fi
+if [ $output_err -ne 0 ]; then
+    echo -e "      Output error: ${RED}${output_err}${RESET}/${sum}"
+fi
+if [ $err -ne 0 ]; then
+    echo - - - - - - - - - - - - - - - - - - -
+    echo -e "      Total errors: ${RED}${err}${RESET}/${sum}"
+fi
+echo ======================================
+
+if [ $err -ne 0 ]; then
+    exit 1
+fi
 
