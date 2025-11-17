8ab557379dab7bce81f6e30342b24d9dc5ef82fa
Michal Šebesta <lachim005@seznam.cz>
2025-11-17 00:00:28 +0000

    More verbose errors

--- main.c
+++ main.c
@@ -12,6 +12,30 @@
 #include "parser.h"
 #include "lexer.h"
 #include "optimizer.h"
+#include <stdio.h>
+
+void print_error_code(ErrorCode ec) {
+    switch (ec) {
+    case OK:
+        fprintf(stderr, "OK"); break;
+    case LEXICAL_ERROR:
+        fprintf(stderr, "lexical error"); break;
+    case SYNTACTIC_ERROR:
+        fprintf(stderr, "syntax error"); break;
+    case SEM_UNDEFINED:
+        fprintf(stderr, "undefined symbol"); break;
+    case SEM_REDEFINITION:
+        fprintf(stderr, "redefined symbol"); break;
+    case SEM_BAD_PARAMS:
+        fprintf(stderr, "incorrect parameter count or type"); break;
+    case SEM_TYPE_COMPAT:
+        fprintf(stderr, "incorrect type in expression"); break;
+    case SEM_OTHER:
+        fprintf(stderr, "unknown semantic error"); break;
+    case INTERNAL_ERROR:
+        fprintf(stderr, "internal error"); break;
+    }
+}
 
 int main() {
     Lexer lexer;
@@ -23,7 +47,9 @@ int main() {
 
     lexer_free(&lexer);
     if (ec != OK) {
-        fprintf(stderr, "Error: %d\n", ec);
+        fprintf(stderr, "error at %d:%d: ", lexer.pos_line, lexer.pos_char);
+        print_error_code(ec);
+        fprintf(stderr, "\n");
         ast_free(ast_root);
         symtable_free(glob_symtable);
         return ec;
@@ -33,12 +59,23 @@ int main() {
 
     ec = optimize_ast(ast_root, glob_symtable);
     if (ec != OK) {
+        fprintf(stderr, "error: ");
+        print_error_code(ec);
+        fprintf(stderr, "\n");
         ast_free(ast_root);
         symtable_free(glob_symtable);
         return ec;
     }
 
-    generate_code(stdout, ast_root, glob_symtable);
+    ec = generate_code(stdout, ast_root, glob_symtable);
+    if (ec != OK) {
+        fprintf(stderr, "error: ");
+        print_error_code(ec);
+        fprintf(stderr, "\n");
+        ast_free(ast_root);
+        symtable_free(glob_symtable);
+        return ec;
+    }
 
     ast_free(ast_root);
     symtable_free(glob_symtable);
